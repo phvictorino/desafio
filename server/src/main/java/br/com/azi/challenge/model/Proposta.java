1848666c379797137fc9b69336561cdc7ce74819
Victorino <pedro.victorino@gmail.com>
2019-11-14 23:15:19 -0400

    edição da model proposta - adicionando getters e setters

--- server/src/main/java/br/com/azi/challenge/model/Proposta.java
+++ server/src/main/java/br/com/azi/challenge/model/Proposta.java
@@ -15,4 +15,60 @@ public class Proposta {
     public String dataCadastro;
     public Integer classificacao;
     public Licitacao licitacao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public BigDecimal getNota() {
+        return nota;
+    }
+
+    public void setNota(BigDecimal nota) {
+        this.nota = nota;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Integer getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(Integer classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public Licitacao getLicitacao() {
+        return licitacao;
+    }
+
+    public void setLicitacao(Licitacao licitacao) {
+        this.licitacao = licitacao;
+    }
 }
